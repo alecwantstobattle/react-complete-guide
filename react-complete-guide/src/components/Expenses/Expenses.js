f739dfd6660c3dfc8150a7f49e7375174f85a291
Alec Tecson <tecsonalr@gmail.com>
2021-08-30 13:18:53 +0800

    Adding Conditional Return Statements

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -15,19 +15,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense, index) => (
-      <ExpenseItem
-        key={expense.id} // can use index if no id, but not recommended
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className='expenses'>
@@ -35,20 +22,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onExpenseFilter={expenseFilterHandler}
         />
-        {
-          /* {filteredExpenses.length === 0 && <p>No expenses found</p>}
-        {filteredExpenses.length > 0 &&
-          // filteredExpenses.length === 0 ? <p>No expenses found</p> :
-          filteredExpenses.map((expense, index) => (
-            <ExpenseItem
-              key={expense.id} // can use index if no id, but not recommended
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))} */
-          expensesContent
-        }
+        <ExpensesList expenses={filteredExpenses} />
       </Card>
     </div>
   );
