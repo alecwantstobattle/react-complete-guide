86c834f41f98b21d9262cc45c3a37b1827379c3d
Alec Tecson <tecsonalr@gmail.com>
2021-09-18 21:29:26 +0800

    A Closer Look At Child Component Re-Evaluation

--- bts-and-optimization-techniques/src/components/UI/Button/Button.js
+++ bts-and-optimization-techniques/src/components/UI/Button/Button.js
@@ -3,13 +3,13 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const Button = (props) => {
+  console.log('Button RUNNING');
   return (
     <button
       type={props.type || 'button'}
       className={`${classes.button} ${props.className}`}
       onClick={props.onClick}
-      disabled={props.disabled}
-    >
+      disabled={props.disabled}>
       {props.children}
     </button>
   );
