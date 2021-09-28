839b5e44e7a1578579aa1ed953a08a141333f586
Alec Tecson <tecsonalr@gmail.com>
2021-09-28 19:18:31 +0800

    Advance Redux - Refresher / Practice: Part 2/2

--- advanced-redux/src/store/cart-slice.js
+++ advanced-redux/src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-createSlice({
+const cartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [],
@@ -11,9 +11,11 @@ createSlice({
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
 
+      state.totalQuantity++;
+
       if (!existingItem) {
-        state.item.push({
-          itemId: newItem.id,
+        state.items.push({
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -24,6 +26,22 @@ createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemToCart(state, action) {},
+    removeItemToCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+
+      state.totalQuantity--;
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
