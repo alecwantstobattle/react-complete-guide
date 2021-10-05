db0e4bdda5c4791a8d59dacf20ed408e20e6b958
Alec Tecson <tecsonalr@gmail.com>
2021-10-06 02:14:21 +0800

    Authentication - Persisting The User Authentication Status

--- authentication-react/src/store/auth-context.js
+++ authentication-react/src/store/auth-context.js
@@ -8,16 +8,19 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = React.useState(null);
+  const initialToken = localStorage.getItem('token');
+  const [token, setToken] = React.useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem('token', token);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem('token');
   };
 
   const contextValue = {
