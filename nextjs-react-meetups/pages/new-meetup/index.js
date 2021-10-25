e3308cfd9c9de196723d27866f2a7ac68ab59d74
Alec Tecson <tecsonalr@gmail.com>
2021-10-25 23:30:05 +0800

    Nextjs - React Meetups - Adding the New Meetup Form

--- nextjs-react-meetups/pages/new-meetup/index.js
+++ nextjs-react-meetups/pages/new-meetup/index.js
@@ -1,1 +1,11 @@
 // our-domain.com/new-meetup
+
+function NewMeetupPage() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+
+  return <NewMeetupForm onMeetupHandler={addMeetupHandler} />;
+}
+
+export default NewMeetupPage;
