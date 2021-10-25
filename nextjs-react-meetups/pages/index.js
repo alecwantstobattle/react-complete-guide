e53ac3101a9de1d4766ccce297bd4d141074ff2f
Alec Tecson <tecsonalr@gmail.com>
2021-10-26 00:46:58 +0800

    Nextjs - React Meetups - Exploring Server-side Rendering (SSR) with "getServerSideProps"

--- nextjs-react-meetups/pages/index.js
+++ nextjs-react-meetups/pages/index.js
@@ -24,6 +24,17 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: DUMMY_MEETUPS,
+//   };
+// }
+
 export async function getStaticProps() {
   // fetch data from API
   return {
