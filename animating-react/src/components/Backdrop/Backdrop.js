027ddb698c9309c4583b72c103f7578048ad5be9
Alec Tecson <tecsonalr@gmail.com>
2021-11-01 14:25:17 +0800

    Animating React - Preparing the Project

---
+++ animating-react/src/components/Backdrop/Backdrop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Backdrop.css';
+
+const backdrop = (props) => {
+  const ccsClasses = [
+    'Backdrop',
+    props.show ? 'BackdropOpen' : 'BackdropClosed',
+  ];
+
+  return <div className={ccsClasses.join(' ')}></div>;
+};
+
+export default backdrop;
