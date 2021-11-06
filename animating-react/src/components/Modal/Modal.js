d5661e284b54a2ea8edc6650e5af7758ac7e9b55
Alec Tecson <tecsonalr@gmail.com>
2021-11-06 15:36:40 +0800

    Animating React - Using the Transition Component

--- animating-react/src/components/Modal/Modal.js
+++ animating-react/src/components/Modal/Modal.js
@@ -3,7 +3,14 @@ import React from 'react';
 import './Modal.css';
 
 const modal = (props) => {
-  const ccsClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
+  const ccsClasses = [
+    'Modal',
+    props.show === 'entering'
+      ? 'ModalOpen'
+      : props.show === 'exiting'
+      ? 'ModalClosed'
+      : null,
+  ];
 
   return (
     <div className={ccsClasses.join(' ')}>
