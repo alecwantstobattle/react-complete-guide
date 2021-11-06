624edf77b0db256037edcb740abf38246e58d719
Alec Tecson <tecsonalr@gmail.com>
2021-11-06 15:46:49 +0800

    Animating React - Wrapping the Transition Component

--- animating-react/src/components/Modal/Modal.js
+++ animating-react/src/components/Modal/Modal.js
@@ -1,24 +1,30 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
 const modal = (props) => {
-  const ccsClasses = [
-    'Modal',
-    props.show === 'entering'
-      ? 'ModalOpen'
-      : props.show === 'exiting'
-      ? 'ModalClosed'
-      : null,
-  ];
-
   return (
-    <div className={ccsClasses.join(' ')}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <Transition mountOnEnter unmountOnExit in={props.show} timeout={300}>
+      {(state) => {
+        const ccsClasses = [
+          'Modal',
+          state === 'entering'
+            ? 'ModalOpen'
+            : state === 'exiting'
+            ? 'ModalClosed'
+            : null,
+        ];
+        return (
+          <div className={ccsClasses.join(' ')}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 
