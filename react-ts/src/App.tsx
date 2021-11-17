77f45acae1cf57faf757681e03d075e02e5df82d
Alec Tecson <tecsonalr@gmail.com>
2021-11-18 01:53:27 +0800

    React + TypeScript - Working with "Function Props"

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,11 +1,15 @@
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todos';
 
 function App() {
   const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
 
+  const addTodoHandler = (text: string) => {};
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
